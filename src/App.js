25287687e494f85a11b8bac99e7366971f22a98d
phichidung <benevolentwarriors@gmail.com>
2019-10-03 12:52:50 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 import StylingContext from './styling/context';
 import style from './styling/style';
-import MyComponent from "./MyComponent";
-
-class FooBar extends Component {
-
-    render() {
-        return (
-            <MyComponent/>
-        )
-    }
-}
+import Foobar from "./Foobar";
 
 export default class App extends Component {
     render() {
         return (
             <div>
                 <StylingContext.Provider value={style}>
-                    <FooBar/>
+                    <Foobar/>
                 </StylingContext.Provider>
             </div>
         )
---
+++ src/Foobar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import MyComponent from "./MyComponent";
+
+export default class Foobar extends Component {
+
+    render() {
+        return (
+            <MyComponent/>
+        );
+    }
+}
