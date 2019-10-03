0ca45a3d5d0dc33bf3ac3c3553fbcc3ced2a26d1
phichidung <benevolentwarriors@gmail.com>
2019-10-03 13:04:24 +0700

    covert code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import StylingContext from './styling/context';
 import style from './styling/style';
-import Foobar from "./Foobar";
+import Foobar from './Foobar';
 
 export default class App extends Component {
+
     render() {
+
         return (
             <div>
                 <StylingContext.Provider value={style}>
--- src/Foobar.js
+++ src/Foobar.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
-import MyComponent from "./MyComponent";
+import StylingContext from './styling/context';
+
+export class MyComponent extends Component {
+
+    static contextType = StylingContext;
+
+    render() {
+
+        return (
+            <p style={this.context.MyComponent}>
+                Some text
+            </p>
+        )
+    }
+}
 
 export default class Foobar extends Component {
 
     render() {
+
         return (
             <MyComponent/>
         );
